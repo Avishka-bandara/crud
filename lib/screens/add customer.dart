a6929751cb2322db74391ec985d1d8c47e2e063c
AvishkaBandara <21ug0225@sltc.edu.lk>
2024-08-01 10:20:07 +0530

    Update add customer.dart
    add customer changed

--- lib/screens/add customer.dart
+++ lib/screens/add customer.dart
@@ -12,140 +12,180 @@ class AddCustomerScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Add Customer'),
-          centerTitle: true,
-        ),
-        body: Padding(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Customer'),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Padding(
           padding: EdgeInsets.all(16.0),
           child: Form(
             key: _formKey,
             child: SingleChildScrollView(
-                child: Column(
-              children: <Widget>[
-                SizedBox(height: 20),
-                TextFormField(
-                  controller: _firstNameController,
-                  decoration: InputDecoration(
-                    labelText: 'First Name',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8))),
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 20),
+                  TextFormField(
+                    controller: _firstNameController,
+                    decoration: InputDecoration(
+                      labelText: 'First Name',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter first name';
+                      }
+                      return null;
+                    },
                   ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter first name';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 20),
-                TextFormField(
-                  controller: _lastNameController,
-                  decoration: InputDecoration(
-                    labelText: 'Last Name',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8))),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    controller: _lastNameController,
+                    decoration: InputDecoration(
+                      labelText: 'Last Name',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter last name';
+                      }
+                      return null;
+                    },
                   ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter last name';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 20),
-                TextFormField(
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8))),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter email';
+                      } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+')
+                          .hasMatch(value)) {
+                        return 'Please enter a valid email';
+                      }
+                      return null;
+                    },
                   ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter email';
-                    } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+')
-                        .hasMatch(value)) {
-                      return 'Please enter a valid email';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 20),
-                TextFormField(
-                  controller: _phoneController,
-                  decoration: InputDecoration(
-                    labelText: 'Phone',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8))),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    controller: _phoneController,
+                    decoration: InputDecoration(
+                      labelText: 'Phone',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                      ),
+                    ),
+                    keyboardType: TextInputType.number,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter phone number';
+                      } else if (!RegExp(r'^\d{10}$').hasMatch(value)) {
+                        return 'Please enter a valid phone number';
+                      }
+                      return null;
+                    },
                   ),
-                  keyboardType: TextInputType.number,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter phone number';
-                    } else if (!RegExp(r'^\d{10}$').hasMatch(value)) {
-                      return 'Please enter a valid phone number';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 20),
-                TextFormField(
-                  controller: _addressController,
-                  decoration: InputDecoration(
-                    labelText: 'Address',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8))),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    controller: _addressController,
+                    decoration: InputDecoration(
+                      labelText: 'Address',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter address';
+                      }
+                      return null;
+                    },
                   ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter address';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
                       backgroundColor: Color.fromARGB(255, 30, 114, 194),
-                      fixedSize: const Size(300, 45)),
-                  onPressed: () async {
-                    if (_formKey.currentState!.validate()) {
-                      final customerData = {
-                        'firstName': _firstNameController.text,
-                        'lastName': _lastNameController.text,
-                        'email': _emailController.text,
-                        'phone': _phoneController.text,
-                        'address': _addressController.text,
-                      };
+                      fixedSize: const Size(300, 45),
+                    ),
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        final customerData = {
+                          'firstName': _firstNameController.text,
+                          'lastName': _lastNameController.text,
+                          'email': _emailController.text,
+                          'phone': _phoneController.text,
+                          'address': _addressController.text,
+                        };
 
-                      await context
-                          .read<CustomerProvider>()
-                          .addCustomer(customerData);
+                        await context
+                            .read<CustomerProvider>()
+                            .addCustomer(customerData);
 
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text('Registration successful'),
-                          duration: Duration(seconds: 2),
-                          backgroundColor: Color.fromARGB(255, 30, 114, 194),
-                        ),
-                      );
-                      _firstNameController.clear();
-                      _lastNameController.clear();
-                      _emailController.clear();
-                      _phoneController.clear();
-                      _addressController.clear();
-                    }
-                  },
-                  child: Text(
-                    'Register',
-                    style: TextStyle(color: Colors.white, fontSize: 15),
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('Registration successful'),
+                            duration: Duration(seconds: 2),
+                            backgroundColor: Color.fromARGB(255, 30, 114, 194),
+                            behavior: SnackBarBehavior.floating,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                        );
+
+                        _firstNameController.clear();
+                        _lastNameController.clear();
+                        _emailController.clear();
+                        _phoneController.clear();
+                        _addressController.clear();
+                      }
+                    },
+                    child: Text(
+                      'Register',
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    ),
                   ),
-                ),
-              ],
-            )),
+                  SizedBox(height: 20),
+                  Divider(),
+                  Consumer<CustomerProvider>(
+                    builder: (context, provider, child) {
+                      if (provider.customers.isEmpty) {
+                        return Center(child: Text('No customers found.'));
+                      }
+
+                      return Column(
+                        children: provider.customers.map((customer) {
+                          return ListTile(
+                            title: Text(
+                                '${customer['firstName']} ${customer['lastName']}'),
+                            subtitle: Text('Email: ${customer['email']}\n'
+                                'Phone: ${customer['phone']}\n'
+                                'Address: ${customer['address']}'),
+                            onTap: () {
+                              _firstNameController.text = customer['firstName'];
+                              _lastNameController.text = customer['lastName'];
+                              _emailController.text = customer['email'];
+                              _phoneController.text = customer['phone'];
+                              _addressController.text = customer['address'];
+                            },
+                          );
+                        }).toList(),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
